de402ac74280ac0e3cfb0ce94b7d0e0a959c28c1
kosu <kosumon27@gmail.com>
2022-05-26 20:17:35 +0900

    ➕ JEST

---
+++ jest.config.js
@@ -0,0 +1,18 @@
+const nextJest = require("next/jest");
+
+// next.config.jsとテスト環境用の.envファイルが配置されたディレクトリをセット。基本は"./"で良い。
+const createJestConfig = nextJest({ dir: "./" });
+
+// Jestのカスタム設定を設置する場所。従来のプロパティはここで定義。
+const customJestConfig = {
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
+  modulePathIgnorePatterns: ["<rootDir>/e2e/"],
+  // jest.setup.jsを作成する場合のみ定義。
+  // setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
+  moduleNameMapper: { "src/(.*)": "<rootDir>/src/$1" },
+  moduleDirectories: ["node_modules", "<rootDir>/"],
+  testEnvironment: "jest-environment-jsdom",
+};
+
+// createJestConfigを定義することによって、本ファイルで定義された設定がNext.jsの設定に反映されます
+module.exports = createJestConfig(customJestConfig);
