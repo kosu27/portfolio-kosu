8214057ac2c4dd7be6a1ab2997df018bea032d63
kosu <kosumon27@gmail.com>
2022-05-28 08:03:47 +0900

    ➕ graphql/apollo

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,14 @@
 import "../src/style/index.css";
 
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 import { MantineProvider } from "@mantine/core";
 import type { CustomAppPage } from "next/app";
 import Head from "next/head";
+
+const client = new ApolloClient({
+  uri: "https://48p1r2roz4.sse.codesandbox.io",
+  cache: new InMemoryCache(),
+});
 
 const App: CustomAppPage = ({ Component, pageProps }) => {
   const getLayout =
@@ -16,9 +22,11 @@ const App: CustomAppPage = ({ Component, pageProps }) => {
       <Head>
         <title>ToiletMap</title>
       </Head>
-      <MantineProvider withGlobalStyles withNormalizeCSS>
-        {getLayout(<Component {...pageProps} />)}
-      </MantineProvider>
+      <ApolloProvider client={client}>
+        <MantineProvider withGlobalStyles withNormalizeCSS>
+          {getLayout(<Component {...pageProps} />)}
+        </MantineProvider>
+      </ApolloProvider>
     </>
   );
 };
