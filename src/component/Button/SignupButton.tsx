9ce54e69ae8b3adc3c94176d82f8b0120802a098
kosu <kosumon27@gmail.com>
2022-06-02 15:34:35 +0900

    ✏️ ファイル名変更

--- src/component/Button/SignupButton.tsx
+++ src/component/Button/SignupButton.tsx
@@ -1,20 +1,21 @@
+import { Button } from "@mantine/core";
 import type { FC } from "react";
 import { useIsDesktop } from "src/utils/customHooks";
 
-import { Button } from "./Button";
-
-export type LoginButtonProps = {
+export type SignupButtonProps = {
   text: string;
   color: string;
   bg: string;
+  mr?: string;
+  borderColor?: string;
   hover?: {
     color?: string;
     bg?: string;
   };
-  isDisabled: boolean;
+  isDisabled?: boolean;
 };
 
-const LoginButton: FC<LoginButtonProps> = (props: LoginButtonProps) => {
+const SignupButton: FC<SignupButtonProps> = (props: SignupButtonProps) => {
   const { hover, text, ...inputProps } = props;
   const isDesktop = useIsDesktop();
   const isClient = () => {
@@ -24,7 +25,7 @@ const LoginButton: FC<LoginButtonProps> = (props: LoginButtonProps) => {
   return (
     <>
       {isClient() && (
-        <Button size={isDesktop ? "md" : "sm"} _hover={hover} {...inputProps}>
+        <Button size={isDesktop ? "md" : "sm"} {...inputProps} {...hover}>
           {text}
         </Button>
       )}
@@ -32,4 +33,4 @@ const LoginButton: FC<LoginButtonProps> = (props: LoginButtonProps) => {
   );
 };
 
-export { LoginButton };
+export { SignupButton };
