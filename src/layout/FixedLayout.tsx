0d82cee7aa63a5298a0fe83e1e85441fdb055b42
kosu <kosumon27@gmail.com>
2022-06-15 17:22:30 +0900

    🔥

--- src/layout/FixedLayout.tsx
+++ src/layout/FixedLayout.tsx
@@ -1,7 +1,7 @@
 import type { CustomLayout } from 'next'
 
 import { Footer } from './Footer'
-import { Header } from './Header'
+// import { Header } from './Header'
 import { LayoutErrorBoundary } from './LayoutErrorBoundary'
 
 /**
@@ -10,9 +10,7 @@ import { LayoutErrorBoundary } from './LayoutErrorBoundary'
 export const FixedLayout: CustomLayout = (page) => {
   return (
     <div className="flex flex-col mx-auto max-w-screen-md min-h-screen">
-      <header>
-        <Header />
-      </header>
+      <header>{/* <Header /> */}</header>
       <main className="flex-1 bg-blue-300">
         <LayoutErrorBoundary>{page}</LayoutErrorBoundary>
       </main>
