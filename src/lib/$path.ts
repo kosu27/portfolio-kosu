08ef0beaa90e0ab1a5ff4abfeb1890a6ad29808f
kosu <kosumon27@gmail.com>
2022-07-06 23:31:32 +0900

    🚚 signin  path

--- src/lib/$path.ts
+++ src/lib/$path.ts
@@ -2,6 +2,9 @@ export const pagesPath = {
   "about": {
     $url: (url?: { hash?: string }) => ({ pathname: '/about' as const, hash: url?.hash })
   },
+  "sign_in": {
+    $url: (url?: { hash?: string }) => ({ pathname: '/sign-in' as const, hash: url?.hash })
+  },
   "sign_up": {
     $url: (url?: { hash?: string }) => ({ pathname: '/sign-up' as const, hash: url?.hash })
   },
