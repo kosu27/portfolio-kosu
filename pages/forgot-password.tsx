c093270ba03a17e48cdf192f4e35477cf664dcf5
kosu <kosumon27@gmail.com>
2022-07-07 02:03:07 +0900

    ✨ パスワード管理画面作成

---
+++ pages/forgot-password.tsx
@@ -0,0 +1,79 @@
+/* eslint-disable no-console */
+import {
+  Anchor,
+  Box,
+  Button,
+  Center,
+  createStyles,
+  Group,
+  Paper,
+  Text,
+  TextInput,
+  Title,
+} from '@mantine/core'
+import type { CustomNextPage } from 'next'
+import Link from 'next/link'
+import { ArrowLeft } from 'tabler-icons-react'
+
+import { AuthLayout } from '@/layout/AuthLayout'
+import { getPath } from '@/lib/const'
+
+const useStyles = createStyles((theme) => {
+  return {
+    title: {
+      fontSize: 26,
+      fontWeight: 900,
+      fontFamily: `Greycliff CF, ${theme.fontFamily}`,
+    },
+    controls: {
+      [theme.fn.smallerThan('xs')]: {
+        flexDirection: 'column-reverse',
+      },
+    },
+    control: {
+      [theme.fn.smallerThan('xs')]: {
+        width: '100%',
+        textAlign: 'center',
+      },
+    },
+  }
+})
+
+const ForgotPassword: CustomNextPage = () => {
+  const { classes } = useStyles()
+  const handleClick = () => {
+    console.log('clicked')
+  }
+
+  return (
+    <>
+      <Title className={classes.title} align="center">
+        パスワードリセット
+      </Title>
+      <Text color="dimmed" size="sm" align="center">
+        リセットするためにメールアドレスをご入力ください
+      </Text>
+
+      <Paper withBorder shadow="md" p={30} radius="md" mt="xl">
+        <TextInput label="メールアドレス" placeholder="test@example.com" size="md" required />
+        <Group position="apart" mt="lg" className={classes.controls}>
+          <Link href={getPath('SIGN_IN')} passHref>
+            <Anchor<'a'> color="dimmed" size="sm" className={classes.control}>
+              <Center inline>
+                <ArrowLeft size={12} />
+                <Box ml={5}>ログインページに戻る</Box>
+              </Center>
+            </Anchor>
+          </Link>
+          <Button className={classes.control} onClick={handleClick}>
+            リセットする
+          </Button>
+        </Group>
+      </Paper>
+    </>
+  )
+}
+
+ForgotPassword.getLayout = AuthLayout
+
+export default ForgotPassword
