567939ca7eae1d007eefb7cdb0bc09110e096613
kosu <kosumon27@gmail.com>
2022-07-09 11:19:33 +0900

    🔥 不要なコードの削除

--- src/layout/index.ts
+++ src/layout/index.ts
@@ -1,4 +1,2 @@
 export { AuthLayout } from './AuthLayout'
 export { DashboardLayout } from './DashboardLayout'
-export { FixedLayout } from './FixedLayout'
-export { FluidLayout } from './FluidLayout'
